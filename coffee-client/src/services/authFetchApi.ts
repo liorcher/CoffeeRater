68bfc989b14b0609f2f354055c0c1d7976db7240
Her Name <you@example.com>
2024-07-28 19:12:28 +0300

    few things

---
+++ coffee-client/src/services/authFetchApi.ts
@@ -0,0 +1,78 @@
+
+const BASE_URL = 'https://localhost:9000/api/v1'
+
+const uploadImage = async (image: File) => {
+    const response = await fetch(`${BASE_URL}/images/upload`, {
+        body: image,
+        method: "POST"
+    });
+
+    if (!response.ok) {
+        console.log(response)
+        throw new Error('Failed to upload image');
+    }
+
+    return await response.json()
+}
+
+export const loginWithGoogle = async () => {
+    const response = await fetch(`${BASE_URL}/auth/google`);
+    if (!response.ok) {
+        console.log(response)
+        throw new Error('Failed to logout');
+    }
+    let response_json = await response.json()
+    return response_json;
+};
+
+export const login = async (email: string, password: string) => {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+        body: JSON.stringify({email, password: btoa(password)}),
+        method: "POST"
+    });
+    if (!response.ok) {
+        console.log(response)
+        throw new Error('Failed to logout');
+    }
+    let response_json = await response.json()
+    return response_json;
+};
+
+export const getUserDetails = async () => {
+    const response = await fetch(`${BASE_URL}/users/details`);
+    if (!response.ok) {
+        console.log(response)
+        throw new Error('Failed to fetch user details');
+    }
+    let response_json = await response.json()
+    return response_json;
+};
+
+export const logout = async () => {
+    const response = await fetch(`${BASE_URL}/auth/logout`);
+    if (!response.ok) {
+        console.log(response)
+        throw new Error('Failed to logout');
+    }
+    let response_json = await response.json()
+    return response_json;
+};
+
+export const signUp = async (username: string, avatarUrl: string, password: string, email: string) => {
+    // const responseUpload = await uploadImage(image);
+
+    const response = await fetch(`${BASE_URL}/auth/signup`, {
+        body: JSON.stringify({
+            username,
+            password,
+            email,
+            avatarUrl
+        }),
+        method: "POST"
+    });
+
+    if (!response.ok) {
+        console.log(response)
+        throw new Error('Failed to sign up');
+    }
+}
